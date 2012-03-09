572ac11241be7c6918d64631dca1843b9973c6d3
Peter Heise <pheise@koprolalie.com>
2012-03-09 16:50:33 +0100

    Fixed static base_url in test settings

--- pyazure/tests/settings.py
+++ pyazure/tests/settings.py
@@ -4,13 +4,13 @@ AZURE_FILES = {
     'account_name': test_settings.AZURE_ACCOUNT_NAME,
     'key': test_settings.AZURE_ACCOUNT_KEY,
     'container_name': 'test-container',
-    'base_url': 'https://kikoriki.blob.core.windows.net/test-container/',
+    'base_url': 'https://%s.blob.core.windows.net/test-container/' % test_settings.AZURE_ACCOUNT_NAME,
 }
 
 PRIVATE_AZURE_FILES = {
     'account_name': test_settings.AZURE_ACCOUNT_NAME,
     'key': test_settings.AZURE_ACCOUNT_KEY,
     'container_name': 'test-private-container',
-    'base_url': 'https://kikoriki.blob.core.windows.net/test-private-container/',
+    'base_url': 'https://%s.blob.core.windows.net/test-private-container/' % test_settings.AZURE_ACCOUNT_NAME,
     'is_private': True,
 }
